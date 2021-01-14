0550daade40c40efff70749c10fc4a13c55ea1b4
sigflysea <junfree@gmail.com>
2021-01-14 16:09:24 -0600

    change to see database

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -23,6 +23,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
                 {posts.map((post) => (
                     <PostItem key={post._id} post={post} />
                 ))}
+                {console.log('t')}
             </div>
         </Fragment>
     );
