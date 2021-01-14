39eb61757755f1900ebeadc182f26ce4aad95cc1
sigflysea <junfree@gmail.com>
2021-01-14 15:45:24 -0600

    change server.js

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ connectDB();
 
 app.use(express.json({ extented: false }));
 
-app.get('/', (req, res) => {
-    res.send('API ');
-});
+// app.get('/', (req, res) => {
+//     res.send('API ');
+// });
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
